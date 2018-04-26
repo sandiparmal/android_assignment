c0e1c0d61763d84c666429ab0c7434e698c96114
Sandeep <armalsandip@gmail.com>
2018-04-26 23:33:28 +0530

    Initial Android Assignment Project Setup

---
+++ app/src/main/java/infosys/com/androidassignment/CountryDetailsActivity.java
@@ -0,0 +1,13 @@
+package infosys.com.androidassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountryDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_details);
+    }
+}
